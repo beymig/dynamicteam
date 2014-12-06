352f9c606775ec1be7832886db0bd4cce5babcf0
Zhenzhen Jiang <zhenzhen.jiang@gmail.com>
2014-12-06 00:41:39 -0500

    On branch master 	modified:   design/MultiToOne.jsx

--- design/MultiToOne.jsx
+++ design/MultiToOne.jsx
@@ -1,49 +1,53 @@
 ﻿// JavaScript Document
 //Set up vairaibles
 
-var RectPiece = function(filename){
-  this.load = function(filename){
-    var doc = app.activeDocument;
-    var pi = doc.placedItems.add();
-    $.writeln(filename);
-    pi.file = File(filename);
-    this.pi=pi;
-    this.x = pi.controlBounds[0];
-    this.y = pi.controlBounds[1];
-    this.width = pi.width;
-    this.height = pi.height;
+function Rect(ltwh){
+  this.l = ltwh[0];
+  this.t = ltwh[1];
+  this.r = ltwh[0]+ltwh[2];
+  this.b = ltwh[1]-ltwh[3];
+
+  this.w = ltwh[2];
+  this.h = ltwh[3];
+
+  this.get_copy_at = function(point){
+    return new Rect([point[0], point[1], this.w, this.h]);
   };
 
-  this.move_to = function(point){
-    $.writeln("controlBounds:"+this.pi.controlBounds)
-    this.pi.translate(point[0]-this.pi.controlBounds[0], point[1]-this.pi.controlBounds[1])
-    this.x = this.pi.controlBounds[0];
-    this.y = this.pi.controlBounds[1];
-    this.width = this.pi.width;
-    this.height = this.pi.height;
-    $.writeln("After controlBounds:"+this.pi.controlBounds)
+  this.get_ltrb = function(){
+    return [this.l, this.t, this.r, this.b];
   };
 
-  this.inside = function(point){
-    return (point[0]>this.x && point[0]<this.x+this.width) 
-      &&(point[1]>this.y && point[1]<this.y+this.height);
+  this.get_ltwh = function(){
+    return [this.l, this.t, this.w, this.h];
   };
 
-  this.over_lap = function(rect){
+  this.is_overlap = function(rh){
+    var lh = this;
     if (
-        ((rect[0]-this.x)*(rect[0]-(this.x+this.width))>=0)
-        &&((rect[2]-this.x)*(rect[2]-(this.x+this.width))>=0)
-        &&((rect[0]-this.x)*(rect[2]-(this.x+this.width))!=0))
+        ((lh.l-rh.l)*(lh.l-rh.r)>=0)
+        &&((lh.r-rh.l)*(lh.r-rh.r)>=0)
+        &&((lh.l-rh.l)*(lh.r-rh.r)!=0))
         return false;
     if (
-        ((rect[1]-this.y)*(rect[1]-(this.y-this.height))>=0)
-        &&((rect[3]-this.y)*(rect[3]-(this.y-this.height))>=0)
-        &&((rect[1]-this.y)*(rect[3]-(this.y-this.height)))!=0)
+        ((lh.t-rh.t)*(lh.t-rh.b)>=0)
+        &&((lh.b-rh.t)*(lh.b-rh.b)>=0)
+        &&((lh.t-rh.t)*(lh.b-rh.b))!=0)
         return false;
      return true;
-  };
+  }
+}
 
-  this.load(filename);
+function RectPiece(pageitem){
+  var pi = pageitem;
+
+  this.get_rect = function(){
+    return new Rect([pi.controlBounds[0], pi.controlBounds[1], pi.width, pi.height]);
+  }
+
+  this.move_to = function(point){
+    pi.translate(point[0]-pi.controlBounds[0], point[1]-pi.controlBounds[1])
+  };
 }
 
 var PrintBoard = function(artboard){
@@ -52,46 +56,61 @@ var PrintBoard = function(artboard){
   this.insert_points = [];
   this.insert_points.push([1, 1]);
   this.pieces = [];
-  this.insert = function(piece)
+
+  this.insert = function(pageitem)
   {
-    var wh = [piece.width, piece.height];
+    var piece = new RectPiece(pageitem);
+    var rect = piece.get_rect();
     for(var i=0; i<this.insert_points.length; i++)
     {
-      if (this.test(this.insert_points[i], wh))
+      var tr = rect.get_copy_at(this.insert_points[i]);
+      if (this.test(tr))
       {
-        this.place_piece(i, piece);
+        this.insert_points.splice (i, 1);
+        this.place_piece(tr, piece);
         break;
       }
     }
   };
 
-  this.test = function(point, wh)
+  this.test = function(tr)
   {
-    var rect = [point[0], point[1], point[0]+wh[0], point[1]-wh[1]];
-    if (rect[2]>this.width)
+    //var tr = rect.get_copy_at(point);
+    if (tr.r>this.width)
       return false;
     for(var i=0; i<this.pieces.length; i++)
     {
-      if(this.pieces[i].over_lap(rect))
+      if (tr.is_overlap(this.pieces[i].get_rect()))
         return false;
     }
     return true;
   };
 
-  this.place_piece = function(i, piece)
+  this.place_piece = function(rect, piece)
   {
-    var point = this.insert_points[i];
+    var point = [rect.l, rect.t];
     piece.move_to(point);
     this.pieces.push(piece);
-    this.insert_points.splice (i, 1);
-    this.insert_points.push([piece.x+piece.width, piece.y]);
-    this.insert_points.push([piece.x, piece.y-piece.height]);
+    this.insert_points.push([rect.r, rect.t]);
+    this.insert_points.push([rect.l, rect.b]);//piece.y-piece.height]);
     this.insert_points.sort(function(a, b)
       {
         return -(a[1]-b[1])|| (a[0] - b[0]);
       });
   };
 }
+
+function new_artboard_next(artboard){
+  var rect = artboard.artboardRect;
+  rect[0] += rect[2]+100;
+  return artboard.parent.add(rect);
+}
+
+function import_piece(doc, filename){
+  var pi = doc.placedItems.add();
+  pi.file = File(filename);
+  return pi;
+};
 
 // Select the source folder.
 sourceFolder = Folder.selectDialog('Select the folder with Illustrator files that you want to mere into one', '~');
@@ -105,8 +124,9 @@ if (sourceFolder != null) {
   if (files.length > 0) {
     // Get the destination to save the files
     var pb = new PrintBoard(app.activeDocument.artboards[0]);
+    var doc = app.activeDocument;
     for (i = 0; i < files.length; i++) {
-      var piece = new RectPiece(files[i]);
+      var piece = import_piece(doc, files[i]);
       pb.insert(piece);
       //break;
     }
@@ -115,4 +135,27 @@ if (sourceFolder != null) {
     alert('No matching files found');
   }
 }
+
+/*
+var newRect = function(x, y, width, height) {
+    var l = 0;
+    var t = 1;
+    var r = 2;
+    var b = 3;
+ 
+    var rect = [];
+ 
+    rect[l] = x;
+    rect[t] = -y;
+    rect[r] = width + x;
+    rect[b] = -(height - rect[t]);
+ 
+    return rect;
+};
+
+var ab = app.activeDocument.artboards[0];
+var r = ab.artboardRect;
+var r1= newRect(r[0] +r[2]+100, r[1], r[2], -r[3]);
+//
+var nab = app.activeDocument.artboards.add(r1);*/
 
