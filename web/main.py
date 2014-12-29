76987d7099aaf8c11d2eecce7dd0e66fca08ebbf
Zhenzhen Jiang <zhenzhen.jiang@gmail.com>
2014-12-29 16:40:26 -0500

    1. send job button works 	modified:   ../web/main.py 	modified:   ../web/templates/printroom_view.html

--- web/main.py
+++ web/main.py
@@ -20,16 +20,18 @@ class Task(db.Model):
   daystogo = db.Column(db.Date)
   units = db.Column(db.Integer)
   length = db.Column(db.Integer)
+  printer = db.Column(db.String(20))
 
-  def __init__(self, log, fabric=None, daystogo=None, units=None, length=None):
+  def __init__(self, log, fabric=None, daystogo=None, units=None, length=None, printer=None):
     self.log = log
     self.fabric = fabric
     self.daystogo = daystogo
     self.units = units
     self.length = length
+    self.printer = printer
 
   def __repr__(self):
-    return 'id:%d: %s|%s|%s|%d|%d'%(self.id, self.log, self.fabric, self.daystogo, self.units, self.length)
+    return 'id:%d: %s|%s|%s|%d|%d|%s'%(self.id, self.log, self.fabric, self.daystogo, self.units, self.length, self.printer)
 
 
 @app.route('/add', methods=['POST'])
@@ -50,6 +52,13 @@ def hello(name=None):
   tasks = Task.query.order_by(Task.daystogo).all()
   return render_template('task_view.html', tasks=tasks)
 
+@app.route('/sendjob', methods=['POST'])
+def sendjob():
+  taskid, printid = int(request.form['taskid']), request.form['printerid']
+  t = Task.query.get(taskid)
+  t.printer = printid
+  db.session.commit()
+  return redirect(url_for('show_tasks'))
 
 if __name__ == '__main__':
   app.debug = True
