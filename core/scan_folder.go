f11416e566648ed962a3b495c02ba46f42ef96b9
Zhenzhen Jiang <zhenzhen.jiang@gmail.com>
2015-01-03 01:27:37 -0500

    Alter Database structure to support task history.
    	modified:   core/scan_folder.go
    	modified:   web/main.py

--- core/scan_folder.go
+++ core/scan_folder.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"code.google.com/p/gofpdf"
 	"database/sql"
 	"encoding/json"
 	"fmt"
@@ -19,6 +20,18 @@ type Configuration struct {
 	NewTaskFolder     string
 	WaitToPrintFolder string
 	HotFolder         string
+}
+
+type RollInfo struct {
+	Log     string
+	Fabric  string
+	Printer string
+	//Width         int
+	Height     int
+	DaysToGo   time.Time
+	Units      int
+	NumOfTotal int
+	Total      int
 }
 
 func load_config(file string) Configuration {
@@ -64,7 +77,7 @@ func fileEndWith(fname string, s string) bool {
 func scanForNewTask(cfg Configuration, cerr chan error) {
 	//cfg := load_config("cfg.json")
 	src, dst := cfg.NewTaskFolder, cfg.WaitToPrintFolder
-	con, err := sql.Open("mysql", "root:@/dynamicteam")
+	con, err := sql.Open("mysql", "root:@/dynamicteam_test")
 	if err != nil {
 		cerr <- err
 		return
@@ -91,7 +104,7 @@ func scanForNewTask(cfg Configuration, cerr chan error) {
 			length, _ := strconv.Atoi(strings.TrimSuffix(attrs[3], "in"))
 
 			// insert data into database
-			_, err = con.Exec("insert into task (log, fabric, units, length, folderid) values(?, ?, ?, ?, ?)", log, fabric, units, length, folderid)
+			_, err = con.Exec("insert into task (log, fabric, units, daystogo, length, folderid) values(?, ?, ?, ?, ?, ?)", log, fabric, units, time.Now().AddDate(0, 0, 10), length, folderid)
 			if err != nil {
 				cerr <- err
 				continue
@@ -140,7 +153,7 @@ func dispatchPrintJob(cfg Configuration, cerr chan error) {
 	hotfolder := cfg.HotFolder
 	printers := load_printers("printers.json")
 
-	con, err := sql.Open("mysql", "root:@/dynamicteam")
+	con, err := sql.Open("mysql", "root:@/dynamicteam_test?parseTime=true")
 	if err != nil {
 		cerr <- err
 		return
@@ -149,7 +162,7 @@ func dispatchPrintJob(cfg Configuration, cerr chan error) {
 
 	for {
 		// query from db
-		rows, err := con.Query("SELECT id, folderid, printer from task WHERE status='assigned'")
+		rows, err := con.Query("SELECT id, log, fabric, daystogo, units, length, folderid, printer from task WHERE status='assigned'")
 		if err != nil {
 			fmt.Println("Dispatch error:", err)
 			continue
@@ -157,26 +170,57 @@ func dispatchPrintJob(cfg Configuration, cerr chan error) {
 
 	NextRow:
 		for rows.Next() {
-			var id int
-			var folderid, printer string
-			if err = rows.Scan(&id, &folderid, &printer); err != nil || printers[printer] == "" {
+			var id, units, length int
+			var folderid, printer, log, fabric string
+			var daystogo time.Time
+			if err = rows.Scan(&id, &log, &fabric, &daystogo, &units, &length, &folderid, &printer); err != nil || printers[printer] == "" {
 				fmt.Println("Dispatch get value err:", err, printer, "not exist")
 				continue
 			}
 			fmt.Println(id, folderid, printer)
 			// copy files to hotfolder
 			printerFolder := path.Join(hotfolder, printers[printer])
-			pdfFiles, err := ioutil.ReadDir(path.Join(taskfolder, folderid))
+			srcFolder := path.Join(taskfolder, folderid)
+			pdfFiles, err := ioutil.ReadDir(srcFolder)
+
+			// insert cut-sheet
+			cutSheet := path.Join(srcFolder, "task_report.pdf")
+			if err = generateRollReport(RollInfo{
+				Log:      log,
+				Fabric:   fabric,
+				Printer:  printer,
+				Height:   length,
+				DaysToGo: daystogo,
+				Units:    units,
+			}, cutSheet); err != nil {
+				fmt.Println(err)
+				continue NextRow
+			}
+			cutSheetFile, err := os.Stat(cutSheet)
+			if err != nil {
+				fmt.Println(err)
+				continue NextRow
+			}
+
+			pdfFiles = append(pdfFiles, cutSheetFile)
+
 			for _, pdfFile := range pdfFiles {
 				fname := pdfFile.Name()
 				fmt.Println(fname)
-				srcpath := path.Join(taskfolder, folderid, fname)
+				srcpath := path.Join(srcFolder, fname)
 				dstpath := path.Join(printerFolder, fname)
 				if err = copyFile(dstpath, srcpath); err != nil {
 					fmt.Println("Dispatch job failed. ", err, "copy file failed")
 					continue NextRow
 				}
+				_, err = con.Exec("insert into sheet (task_id, src, status) values(?, ?, ?)", id, fname, "dispatched")
+				if err != nil {
+					fmt.Println(err)
+					cerr <- err
+					continue NextRow
+				}
 			}
+
 			if c, err := con.Exec("update task set status='dispatched' where id=?", id); err != nil {
 				fmt.Println(c, err)
 			}
@@ -187,6 +231,36 @@ func dispatchPrintJob(cfg Configuration, cerr chan error) {
 	}
 }
 
+func generateRollReport(rollInfo RollInfo, fpath string) (err error) {
+	var initType gofpdf.InitType
+	var size gofpdf.SizeType
+	size.Wd = 30
+	size.Ht = 5
+	initType.OrientationStr = "P"
+	initType.UnitStr = "in"
+	initType.Size = size
+
+	pdf := gofpdf.NewCustom(&initType)
+	pdf.AddPageFormat("P", size)
+
+	pdf.SetFont("Arial", "B", 36)
+	pdf.Cell(1, 0, fmt.SPrintf("Printer: %s", rollInfo.Printer))
+	pdf.SetXY(0, 1)
+	pdf.Cell(1, 0, "")
+	pdf.Cell(4, 0, fmt.SPrintf("LOG: %s", rollInfo.Log))
+	pdf.Cell(5, 0, fmt.SPrintf("FABRIC: %s-(1/3)", rollInfo.Fabric))
+	pdf.Cell(3, 0, fmt.SPrintf("Uints: %d", rollInfo.Units))
+	pdf.Cell(6, 0, fmt.SPrintf("ROLL LENGTH: %d.IN", rollInfo.Length))
+	pdf.Cell(6, 0, fmt.SPrintf("DaysToGo: %s", rollInfo.DaysToGo.Format("12/Jun/2015")))
+	pdf.SetFont("Arial", "B", 126)
+	pdf.SetXY(8, 3)
+	pdf.Cell(30, 0, "CUT PAPER HERE!")
+	if err = pdf.OutputFileAndClose(fpath); err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+
 func main() {
 	cscan := make(chan error, 20)
 	cdisp := make(chan error, 20)
@@ -195,5 +269,6 @@ func main() {
 	go dispatchPrintJob(cfg, cdisp)
 	<-cscan
 	<-cdisp
+	//generateRollReport(RollInfo{}, "d:\\roll_report.pdf")
 	//fmt.Println("scanForNewTask() returned %v", err)
 }
--- web/main.py
+++ web/main.py
@@ -5,12 +5,12 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from flask.ext.sqlalchemy import SQLAlchemy
 from contextlib import closing
 
-DATABASE = 'flask.db'
+#DATABASE = 'flask.db'
 app = Flask(__name__)
 #app.config.from_object(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, "flask.db")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/dynamicteam'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/dynamicteam_test'
 db = SQLAlchemy(app)
 
 class Task(db.Model):
@@ -23,8 +23,10 @@ class Task(db.Model):
   printer = db.Column(db.String(20))
   status = db.Column(db.String(20))
   folderid = db.Column(db.String(50))
+  create_at = db.Column(db.DateTime, default=db.func.now())
+  modify_at = db.Column(db.DateTime)
 
-  def __init__(self, log, fabric=None, daystogo=None, units=None, length=None, printer=None, status=None, folderid=None):
+  def __init__(self, log, fabric=None, daystogo=None, units=None, length=None, printer=None, status="new", folderid=None):
     self.log = log
     self.fabric = fabric
     self.daystogo = daystogo
@@ -34,9 +36,24 @@ class Task(db.Model):
     self.status = status
     self.folderid = folderid
 
+
   def __repr__(self):
     return 'id:%d: %s|%s|%s|%d|%d|%s'%(self.id, self.log, self.fabric, self.daystogo, self.units, self.length, self.printer)
 
+class Sheet(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  src = db.Column(db.String(120), nullable=False)
+  status = db.Column(db.String(20))
+  task_id = db.Column(db.Integer, db.ForeignKey('task.id'))
+  task = db.relationship("Task", backref=db.backref('sheets', lazy='dynamic'))
+
+  def __init__(self, src, task, status=""):
+    self.src = src
+    self.status = status
+    self.task = task
+
+  def __repr__(self):
+    return "id:%d|%s|%s|%d"%(self.id, self.src, self.status, self.task_id)
 
 @app.route('/add', methods=['POST'])
 def add_task():
@@ -63,6 +80,7 @@ def sendjob():
   taskid, printid = int(request.form['taskid']), request.form['printerid']
   t = Task.query.get(taskid)
   t.printer = printid
+  t.modify_at = datetime.now()
   t.status = "assigned"
   db.session.commit()
   return redirect(url_for('show_tasks'))
