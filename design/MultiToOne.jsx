78ae027f5eee204bd34f1663098be115f1588727
Zhenzhen Jiang <zhenzhen.jiang@gmail.com>
2015-02-17 23:39:09 -0500

    arrange sheet, working with arrange structure

--- design/MultiToOne.jsx
+++ design/MultiToOne.jsx
@@ -2,6 +2,70 @@
 //Set up vairaibles
 #include "m2o_cfg.jsx"
 
+Array.prototype.map = function(f){
+  var arry = [];
+  for ( var i=0; i<this.length; i++){
+    arry.push(f(this[i]));
+  }
+  return arry;
+}
+
+Array.prototype.filter = function(f){
+  var arry = [];
+  for ( var i=0; i<this.length; i++){
+    if(f(this[i])) arry.push(this[i]);
+  }
+  return arry;
+}
+
+Array.prototype.reduce = function(f, initValue){
+  var result = initValue;
+  for ( var i=0; i<this.length; i++){
+    result = f(this[i], result);
+  }
+  return result;
+}
+
+Array.prototype.indexOf = function(v){
+  for ( var i=0; i<this.length; i++){
+    if (this[i]==v){
+      return i;
+    }
+  }
+  return -1;
+}
+
+Array.prototype.locate = function(comp, f){
+  for ( var i = 0; i<this.length; i++){
+    if (f(comp, this[i]))
+      return i;
+  }
+  return -1;
+}
+
+Array.prototype.groupBy = function(keygen){
+  var result = {}
+  for ( var i = 0; i<this.length; i++){
+    var key = keygen(this[i]);
+    if (key in result){
+      result[key].push(this[i]);
+    }else{
+      result[key] = [this[i]];
+    }
+  }
+  return result;
+}
+
+Object.prototype.keys = function(){
+  var keys = [];
+  for ( key in this){
+    if (key != "keys"){
+      keys[keys.length] = key;
+    }
+  }
+  return keys;
+}
+
 // all fabric number -= 2
 function Rect(ltwh){
   this.l = ltwh[0];
@@ -208,8 +272,131 @@ function deleteThruCut(){
 }
 
 
-var Task = function(folder){
+var Task = function(source){
   var _this = this;
+
+  this.execute = function(){
+    // detect task type
+    if (source instanceof Folder){
+      var files = source.getFiles(/\.(ai|eps|pdf)$/i);
+      var attrs = source.displayName.split("_");
+
+      this.log = attrs[0];
+
+      this.size_groups = group_files(files);
+      // [
+      //   ('foldername', [('basename', files), ...])
+      //   ...
+      // ]
+      if ( attrs[1]=="S" || attrs[1]=="L"){
+        // if sheet
+        // split folder by Nothing; split sheet by body size > number; arrange by piece size
+        var sheetGroup = files.map(function(f){
+          var attr = f.displayName.split(".")[0].split("_");
+          if ( attr[0]=="" ){
+            attr = attr.slice(1);
+          }
+          return [attr[0], attr[attr.length-1], f]; //[size, number]
+        }).filter(function(f){
+          return !isNaN(f[1]);
+        }).groupBy(function(item){ return [item[0], item[1]].join('_'); });
+
+
+        this.unit_count = attrs[2];
+        this.filesArrangeStruct = {}
+        this.filesArrangeStruct["Sheet"+attrs[1]]= sheetGroup.keys().sort(function(l, r){
+          var lf = sheetGroup[l];
+          var rf = sheetGroup[r];
+          if (lf[0]!=rf[0]){
+            return size_order.indexOf(rf[0]) - size_order.indexOf(lf[0]);
+          }else{
+            return parseInt(lf[1])-parseInt(rf[1]);
+          }
+        }).map(function(item){return [item, sheetGroup[item]];});
+      }
+      else{
+        // if roll
+        // split folder by fabric; split sheet by body size; sort by piece size
+        this.unit_count = attrs[1];
+        var fabric_groups = files.map(function(f){
+          var attr = f.displayName.split("_");
+          if ( attr[0]=="" ){
+            attr = attr.slice(1);
+          }
+          var size = attr[0];
+          var fabric = attr[1]=="FLAT"?attr[2]:attr[1];
+          fabric = fabric.replace("FLAT", "").replace(/^\s+|\s+$/g, '');
+          return [size, fabric, f]; //[size, number]
+        }).groupBy(function(item){
+          return item[1];
+        });
+        for ( var key in fabric_groups){
+          $.writeln(key);
+        }
+        for ( var key in fabric_groups){
+          if (key=="keys")
+            continue;
+          var size_groups = fabric_groups[key].groupBy(function(item){return item[0];});
+          fabric_groups[key] = size_groups.keys().sort(function(l, r){return size_order.indexOf(r[0][0]) - size_order.indexOf(l[0][0])}).map(function(size){
+            return [size, size_groups[size]];
+          });
+        }
+        this.filesArrangeStruct = fabric_groups;
+      }
+    }
+    else{
+      // redo
+      // split folder by nothing; split sheet by Nothing; arrange sort by body size > piece size
+      source.open('r');
+      var info = source.read();
+      source.close();
+
+      info = info.split(";");
+      folderName = info.shift();
+      var attrs = folderName.split("_");
+
+      files = [];
+      //for ( var i=0; i<info.length; i++)
+      //  files[i] = File(info[i]);
+      files = info.map(function(fname){ return new File(fname);});
+
+      this.log = attrs[0];
+      this.unit_count = attrs[1];
+      var fabric_groups = files.map(function(f){
+        var attr = f.displayName.split("_");
+        if ( attr[0]=="" ){
+          attr = attr.slice(1);
+        }
+        var size = attr[0];
+        var fabric = attr[1]=="FLAT"?attr[2]:attr[1];
+        fabric = fabric.replace("FLAT", "").replace(/^\s+|\s+$/g, '');
+        return [size, fabric, f]; //[size, number]
+      }).groupBy(function(item){
+        return item[1];
+      });
+      for ( var key in fabric_groups){
+        if (key=="keys")
+          continue;
+        var size_groups = fabric_groups[key].groupBy(function(item){return item[0];});
+        fabric_groups[key] = [["mix", size_groups.keys().sort(function(l, r){return size_order.indexOf(r[0][0]) - size_order.indexOf(l[0][0])}).map(function(key){
+          return size_groups[key];
+        })]];
+      }
+      this.filesArrangeStruct = fabric_groups;
+
+
+      /*this.filesArrayStruct = files.map(function(f){
+        var attr = f.displayName.split("_");
+        if ( attr[0]=="" ){
+          attr = attr.slice(1);
+        }
+        var size = attr[0];
+        var fabric = attr[1]=="FLAT"?attr[2]:attr[1];
+        fabric = fabric.replace("FLAT", "").replace(/^\s+|\s+$/g, '');
+        return [size, fabric, f]; //[size, number]
+      }).dictBy([1], [2]) */
+    }
+  };
 
   var group_files = function(files){
     //var re = new RegExp(".*\\.(ai|eps|pdf)$", "i");
@@ -346,6 +533,54 @@ var Task = function(folder){
     return pieces;
   };
 
+  this.export_all_new = function(){
+    var global_seq = 0;
+    var fabrics = this.filesArrangeStruct.keys();
+    var timeStamp = (new Date()).valueOf().toString().substr(5, 5);
+    var sub_folders = {};
+    for (var fi = 0; fi<fabrics.length; fi++){
+      var fabric = fabrics[fi];
+      var folder_id = [this.log, fabric, this.unit_count].join('_');
+      var output_folder = new Folder(OUTPUT_FOLDER + "\\" + [COMBINING_PREFIX, folder_id].join('_'));
+      if ( ! output_folder.exists ){
+        output_folder.create();
+      }
+      sub_folders[folder_id]=0;
+
+      var fabric_width = FABRIC_LIST[fabric][1];
+      var ab = app.activeDocument.artboards[0];
+      $.writeln("fabric: "+fabric + "--> " + fabric_width);
+      resize_artboard(ab, fabric_width-2, 0);
+      var fabric_group = this.filesArrangeStruct[fabric];
+      for ( var i = 0; i < fabric_group.length; i++){
+        // file id = fabric_group[i][0]
+        var files = fabric_group[i][1];
+        var pieces = [];
+        if (files[0][0] instanceof Array){
+          for (var j=0; j<files.length; j++){
+            pieces = pieces.concat(this.importAndOrderBySize(files[j].map(function(item){ return item[2];})));
+          }
+        } else{
+          pieces = this.importAndOrderBySize(files.map(function(item){ return item[2];}));
+        }
+        sub_folders[folder_id]+=this.combinePieces(ab, output_folder, [this.log, fabric, fabric_group[i][0]].join('_'), pieces);
+      }
+    }
+    var subfolderIds = sub_folders.keys();
+    var folder_count = subfolderIds.length;
+    var timeStamp = (new Date()).valueOf().toString().substr(5, 5);
+    for (var i=0; i<folder_count; i++) {
+      folder_id = subfolderIds[i];
+      //var total_length = (new UnitValue(sub_folders[folder_id], "px")).as ('in');
+      var sub_folder = new Folder(OUTPUT_FOLDER + "\\" + [COMBINING_PREFIX, folder_id].join('_'));
+      var ok = sub_folder.rename(OUTPUT_FOLDER + "\\" + [COMBINED_PREFIX, folder_id, sub_folders[folder_id].toString()+"in", (i-1).toString()+"-"+folder_count.toString(), timeStamp].join('_'));
+      if(!ok)
+        $.writeln("Rename failed: "+sub_folder);
+      else
+        $.writeln("Rename to: "+sub_folder);
+    }
+  };
+
   this.export_all = function(){
     var global_seq = 0;
     var sub_folders = {};
@@ -405,7 +640,8 @@ var Task = function(folder){
     }
   };
 
-  init(folder);
+  //init(source);
+  this.execute();
 }
 
 
@@ -442,16 +678,7 @@ function resize_artboard(ab, width, height){
   ab.artboardRect = [0, 0, width, height];
 };
 
-function main(){
-  // Select the source folder.
-  var source = Folder.selectDialog('Select the folder with Illustrator files that you want to mere into one', '~');
-
-  if(!source)
-    source = File.openDialog('Select the folder with Illustrator files that you want to mere into one', '*.redo');
-
-  if(!source)
-    return;
-
+function createTitleText(){
   // Point Text
   var pointTextRef = app.activeDocument.textFrames.add();
   pointTextRef.contents = "";
@@ -480,13 +707,26 @@ function main(){
     charAttr.fillColor = redColor;
   }
   // apply to each textFrame in the document
+  // TODO: replace with pointTextRef
   charStyle.applyTo(docRef.textFrames[0].textRange);
 
+  return pointTextRef;
+}
 
-  CUTCODE_TEXTFRAME = pointTextRef;
+function main(){
+  // Select the source folder.
+  var source = Folder.selectDialog('Select the folder with Illustrator files that you want to mere into one', '~');
+
+  if(!source)
+    source = File.openDialog('Select the folder with Illustrator files that you want to mere into one', '*.redo');
+
+  if(!source)
+    return;
+
+  CUTCODE_TEXTFRAME = createTitleText();
 
   var task = new Task(source);
-  task.export_all();
+  task.export_all_new();
   CUTCODE_TEXTFRAME.remove();
   if (source instanceof File){
     source.rename(source.name.replace(".redo", ".done"));
