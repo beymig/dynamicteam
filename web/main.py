3f0d95b17c3fcfe449bd447a0c778d341e4e7111
unknown <Noemi@DT-ON-GA13.dts.local>
2015-03-18 01:19:04 -0400

    adjust sheet list format

--- web/main.py
+++ web/main.py
@@ -133,7 +133,7 @@ def blank_view():
 @app.route('/sheetlist', methods=['GET'])
 def show_sheet_tasks():
   tasks = Task.query.order_by(Task.log).filter(Task.fabric.startswith("SHEET"))
-  return render_template('list_view.html', title="Sheets List", items=["%s %s"%(t.folderid, t.modify_at) for t in tasks], printbtn=True)
+  return render_template('list_view.html', title="Sheets List", items=["%s %s"%(t.folderid[:-6], t.modify_at) for t in tasks], printbtn=True)
 
 @app.route('/')
 @app.route('/printroom', methods=['GET'])
