3297bb6aadb760c52d729634f7e64e70c92a17c1
Zhenzhen Jiang <zhenzhen.jiang@gmail.com>
2015-03-18 00:24:37 -0400

    adjust content and the style of show it

--- web/main.py
+++ web/main.py
@@ -133,7 +133,7 @@ def blank_view():
 @app.route('/sheetlist', methods=['GET'])
 def show_sheet_tasks():
   tasks = Task.query.order_by(Task.log).filter(Task.fabric.startswith("SHEET"))
-  return render_template('list_view.html', title="Sheets List", items=["%s %s"%(t.log, t.fabric) for t in tasks], printbtn=False)
+  return render_template('list_view.html', title="Sheets List", items=["%s %s"%(t.folderid, t.modify_at) for t in tasks], printbtn=True)
 
 @app.route('/')
 @app.route('/printroom', methods=['GET'])
